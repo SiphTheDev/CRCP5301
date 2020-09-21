34434082bf09dc1ea7f18e85ea0efeb1cab503b4
SiphTheDev <siphimir@povingames.com>
2020-09-20 23:38:12 -0500

    Added comments, refactored enemy methods, and made miniFoe follow mouse on Y axis.

--- III-ProjectProposal/Learning P5JS/HelloWorld/Enemy.js
+++ III-ProjectProposal/Learning P5JS/HelloWorld/Enemy.js
@@ -1,20 +1,36 @@
 class Enemy{
-    constructor (name, speed, farben, x){
+    constructor (name, speed, farben, x, y){
     this.name = name;
     this.speed = speed;
     this.farben = farben;
     this.x = x;
+    this.y = y;
   }
   
+  update(){
+    this.move();
+    this.trackMouse();
+    this.drawFoe();
+  }
   move(){
     this.x += this.speed;
-    if(this.speed > width || this.speed < 0){
+    if(this.x > width || this.x < 0){
       this.speed = -this.speed;
+      this.x += (2 * this.speed);
     }   
+  }
+  
+  trackMouse(){
+    if(this.y > mouseY){
+      this.y--;
+    }
+    else if(this.y < mouseY){
+      this.y++;
+    }
   }
   
   drawFoe(){
     fill(this.farben);
-    ellipse(this.x, 250, 25,25);
+    ellipse(this.x, this.y, 25,25);
   }
 }
--- III-ProjectProposal/Learning P5JS/HelloWorld/HelloWorld.js
+++ III-ProjectProposal/Learning P5JS/HelloWorld/HelloWorld.js
@@ -1,16 +1,17 @@
+//Thomas Park
+//Sep 20, 2020
+//This is just silliness as I get the hang of P5.JS
 function setup() {
 createCanvas(1200,600);
-miniFoe = new Enemy("Jiom", 1, 125, 500);
-//right auti, bottom auto
+miniFoe = new Enemy("Jiom", 5, 180, 500, 500); //initilizes miniFor with a name, speed, color, x pos, and y pos.
+//Look into: right auto, bottom auto for screen formatting?
 }
 
 
 function draw() {
 background(250,150,12);
 //print("Howdy World!");
-//fill(55,250,250);
-fill(255);
+fill(255); 
 ellipse(mouseX,mouseY, 50,50);
-miniFoe.move();
-miniFoe.drawFoe();
+miniFoe.update();
 }
