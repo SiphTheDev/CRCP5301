354c1e87c10458c2d63d01a9ab9997bf85e85c83
SiphTheDev <siphimir@povingames.com>
2020-09-20 19:27:06 -0500

    Practicing p5.js in new environement

---
+++ III-ProjectProposal/Learning P5JS/HelloWorld/HelloWorld.js
@@ -0,0 +1,8 @@
+function setup() {
+createCanvas(500,500);
+}
+
+
+function draw() {
+background(250,150,12);
+}
---
+++ III-ProjectProposal/Learning P5JS/p5.js test programs/empty-example/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  // put setup code here
+  createCanvas(400,400);
+}
+
+function draw() {
+  // put drawing code here
+  background(150);
+}
