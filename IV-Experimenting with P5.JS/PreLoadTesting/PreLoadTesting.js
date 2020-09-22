e97c0be7ba2359976edce5d5e0f8691ac1c1bde2
SiphTheDev <siphimir@povingames.com>
2020-09-21 22:00:10 -0500

    Added a folder to store early experiments with p5.js. Also began preLoad testing

---
+++ IV-Experimenting with P5.JS/PreLoadTesting/PreLoadTesting.js
@@ -0,0 +1,18 @@
+let song;
+
+function preload(){
+song = loadSouns('Abstraction - Three Red Hearts - Puzzle Pieces.wav');
+}
+
+function setup() {
+song.play();
+}
+
+
+function draw() {
+
+}
+
+function mousePressed(){
+
+}
