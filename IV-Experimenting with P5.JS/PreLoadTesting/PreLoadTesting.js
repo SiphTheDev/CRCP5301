50a554a893b928cd912803d4d48ec4ab361f4e41
SiphTheDev <siphimir@povingames.com>
2020-09-21 22:28:07 -0500

    Successfully loaded and displayed/played an image and sound

--- IV-Experimenting with P5.JS/PreLoadTesting/PreLoadTesting.js
+++ IV-Experimenting with P5.JS/PreLoadTesting/PreLoadTesting.js
@@ -1,18 +1,25 @@
 let song;
+let img;
 
 function preload(){
-song = loadSouns('Abstraction - Three Red Hearts - Puzzle Pieces.wav');
+song = loadSound('assets/GDM_Stinger7.mp3');
+img = loadImage('assets/7Souls_Crab1.png');
 }
 
 function setup() {
-song.play();
+song.loop();
+createCanvas(500,500);
+background(0,150,150);
+img.loadPixels();
 }
 
 
 function draw() {
-
+image(img,50,50,50,50);
+//song.play();
 }
 
 function mousePressed(){
-
+song.play();
+background(0);
 }
