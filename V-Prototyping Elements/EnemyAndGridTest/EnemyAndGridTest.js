568579781ce298b793f73981ab62dddd367bd5b8
SiphTheDev <siphimir@povingames.com>
2020-09-27 16:09:57 -0500

    Created new folder for small sketches to test different aspects of the program, such as enemy pathfinding.Also created a new sketch to work on spritesheets

---
+++ V-Prototyping Elements/EnemyAndGridTest/EnemyAndGridTest.js
@@ -0,0 +1,16 @@
+//Enemy & World Grid Test
+
+
+function setup() {
+  createCanvas(800,600);
+  background(0);
+}
+
+
+function draw() {
+        background(0);
+        fill(255);
+      }
+    
+  
+    
