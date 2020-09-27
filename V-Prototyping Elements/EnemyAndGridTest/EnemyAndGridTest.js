9e1b654d62010655661980e65b60e5108dadcbea
SiphTheDev <siphimir@povingames.com>
2020-09-27 17:29:51 -0500

    Worked on Loading Sprites via p5.Play library

--- V-Prototyping Elements/EnemyAndGridTest/EnemyAndGridTest.js
+++ V-Prototyping Elements/EnemyAndGridTest/EnemyAndGridTest.js
@@ -1,16 +1,24 @@
 //Enemy & World Grid Test
 
+//Sprite sheet loading based on example "Animations - Sprite Sheet" from p5.play library resources:
+//Link to example: http://molleindustria.github.io/p5.play/examples/index.html?fileName=animation_sprite_sheet.js
+
+var testSprites;
+var spriteSheetFull;
 
 function setup() {
-  createCanvas(800,600);
+  createCanvas(160,160);
   background(0);
+  
+   testSprites = loadSpriteSheet('assets/TP_SampleSprites.png',16,16,9); //loads the units on the sheet into a sprite sheet object
+   spriteSheetFull = loadImage('assets/TP_SampleSprites.png'); //To see the sheet in its entirety
 }
 
 
 function draw() {
-        background(0);
-        fill(255);
-      }
+  clear(); //I think this resets the screen, rather than drawing over it repeatedly. Allows for animations.
+  image(spriteSheetFull, 20,20,48,48);
+}
     
   
     
