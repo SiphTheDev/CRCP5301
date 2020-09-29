dcb491e23f55b8e0e04d7186e3e4ea8cfa01b7d4
SiphTheDev <siphimir@povingames.com>
2020-09-28 23:43:46 -0500

    Loaded & displayed a background image while retaining its original proportions. Also added a background color & centered the sketch in index.html.

--- V-Prototyping Elements/ImageAndAnimationTest/ImageAndAnimationTest.js
+++ V-Prototyping Elements/ImageAndAnimationTest/ImageAndAnimationTest.js
@@ -1,10 +1,30 @@
-//PImage, Pixel, and Sprite Animaton Test
+/*
+PImage, Pixel, and Sprite Animaton Test
+Sep 28,2020
+Based on the example on Processing.org here: https://processing.org/examples/animatedsprite.html 
+*/
+let backGround;
+
+function preload(){
+  backGround = loadImage('assets/PixelArtInfiniteRunnerPack_EderMuniz/Forest_Background.png');
+}
+
 function setup() {
-createCanvas(500,500);
+createCanvas(950,382);
 background(245);
+//backGround.loadPixels();
 }
 
 
 function draw() {
-
+  //Step 1: Draw the Background.
+  image(backGround, 0, 0, 0, height); //myImage, xPos, yPos, width (zero = scale proportionally), height);
+  
+  
 }
+
+/*
+ToDo in future:
+- Use pixelArray, get, & set to shift the background image to be centered on the original. 
+  
+*/
