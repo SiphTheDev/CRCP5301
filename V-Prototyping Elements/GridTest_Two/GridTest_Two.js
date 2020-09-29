6d8d1abd0b3bb36badf9e2fe7bf2e27e3314bc00
SiphTheDev <siphimir@povingames.com>
2020-09-29 14:31:22 -0500

    With the former error resolved, I'm having another go at p5.play.js. It has some compatibility erros with p5 >5.5, but the gitHub has advice.

--- V-Prototyping Elements/GridTest_Two/GridTest_Two.js
+++ V-Prototyping Elements/GridTest_Two/GridTest_Two.js
@@ -5,15 +5,17 @@ let tileSheet;
 
 
 function preload(){
-  backgroundImg = loadImage('./assets/LavaCavesPackv1/Background/BG_full.png');
-  tileSheet = loadImage('./assets/LavaCavesPackv1/Tileset/Lava_tileset.png');
+  backgroundImg = loadImage('assets/LavaCavesPackv1/Background/BG_full.png');
+  //tileSheet = loadImage('assets/LavaCavesPackv1/Tileset/Lava_tileset.png');
+  lavaTile = loadSpriteSheet('assets/LavaCavesPackv1/Tileset/Lava_tileset.png', 16, 16, 4);
+
 
 }
 
 function setup() {
-  createCanvas(1000,500);
+  createCanvas(1000,750);
   //frameRate(3); //So I can actually see what is happening.
-  
+
  
 }
 
@@ -22,10 +24,11 @@ function draw() {
   //clear(); //Clears the screen between draw calls.
   background(200);
   
-  image(backgroundImg, 0, 0, 150, 150); //myImage, xPos, yPos, width (zero = scale proportionally), height);
+  image(backgroundImg, 0, 0, 1000, 750); //myImage, xPos, yPos, width (zero = scale proportionally), height); //To Scale proport, then grow, scale first, then draw the new grown one. 
+  lavaTile.drawFrame(0,0);
+ 
   
-
-}
-    
+//Don't rescale automatically. Either do it in another program before loading, or else copy stuff over pixel by pixel, 1 to 4 or something (do the first one, not the second).
+}    
   
     
