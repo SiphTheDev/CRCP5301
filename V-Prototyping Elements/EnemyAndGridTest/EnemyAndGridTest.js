82e5c2f80703ddf9a5f09d406639b4ae63321d4d
SiphTheDev <siphimir@povingames.com>
2020-09-29 13:22:16 -0500

    Repurposing this program to test mapping tiles & enemy movement.

--- V-Prototyping Elements/EnemyAndGridTest/EnemyAndGridTest.js
+++ V-Prototyping Elements/EnemyAndGridTest/EnemyAndGridTest.js
@@ -5,23 +5,21 @@ var pyramid;
 var tileSheet;
 
 function preLoad(){
-pyramid = loadAnimation('assets/Tp_TriWalk/Tri_Walk_Side_0000.png','assets/Tp_TriWalk/Tri_Walk_Side_0003.png');
 }
 
 function setup() {
   createCanvas(500,500);
-  //frameRate(3); //So I can actually see what is happening.
+  frameRate(3); //So I can actually see what is happening.
   
  
 }
 
 
 function draw() {
-  //clear(); //Clears the screen between draw calls.
+  clear(); //Clears the screen between draw calls.
   background(245); //an off-white
   
 
-  animation(pyramid, 50,50);
 }
     
   
