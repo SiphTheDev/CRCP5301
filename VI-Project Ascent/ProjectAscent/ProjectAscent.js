24db73bab0bc98e7af4594d88325edb7e3e0c992
SiphTheDev <siphimir@povingames.com>
2020-10-07 23:57:50 -0500

    Created the findBestTile function to determine the tile with the lowest F value.

--- VI-Project Ascent/ProjectAscent/ProjectAscent.js
+++ VI-Project Ascent/ProjectAscent/ProjectAscent.js
@@ -36,7 +36,6 @@ function draw() {
   drawGridArray();
   jim.move();
   jim.render();
- 
 }
 
 function createTileMap() { //temp until put this data in a json or elsewhere. //maybe make this a 2d array in future?
@@ -68,10 +67,10 @@ function loadGridArray() { //Once you change the gridTileMap to a JSON, use type
     for (let c = 0; c < cols; c++) {
       if (gridTileMap[i] == 10) {
         type = 1;
-        farbe = color(255,0,0);
+        farbe = color(255, 0, 0);
       } else if (gridTileMap[i] == 20) {
         type = 2;
-        farbe = color(0,255, 0);
+        farbe = color(0, 255, 0);
       } else if (gridTileMap[i] == 30) {
         type = 3;
         farbe = color(0, 0, 255);
@@ -91,60 +90,79 @@ function drawGridArray() {
 }
 
 function findAdjacent(node) {
-  if (node.r < 13){
+  if (node.r < 13) {
     if (gridArray[node.c][node.r+1].type == 2) { //type 2 = enemy terrain.
       toSearch[toSearch.length] = gridArray[node.c][node.r+1];
       gridArray[node.c][node.r+1].gridFrom = node;
-      gridArray[node.c][node.r+1].farbe = color(0,200,100); // for testing
+      gridArray[node.c][node.r+1].farbe = color(0, 200, 100); // for testing
     }
   }
   if (node.c < 6) {
     if (gridArray[node.c+1][node.r].type == 2) { 
       toSearch[toSearch.length] = gridArray[node.c+1][node.r];
       gridArray[node.c+1][node.r].gridFrom = node;
-      gridArray[node.c+1][node.r].farbe = color(0,200,100); // for testing
+      gridArray[node.c+1][node.r].farbe = color(0, 200, 100); // for testing
     }
   }
   if (node.r > 0) {
     if (gridArray[node.c][node.r-1].type == 2) { 
       toSearch[toSearch.length] = gridArray[node.c][node.r-1];
       gridArray[node.c][node.r-1].gridFrom = node;
-      gridArray[node.c][node.r-1].farbe = color(0,200,100); //for testing
+      gridArray[node.c][node.r-1].farbe = color(0, 200, 100); //for testing
     }
   }
   if (node.c > 0) {
     if (gridArray[node.c-1][node.r].type == 2) {
       toSearch[toSearch.length] = gridArray[node.c-1][node.r];
       gridArray[node.c-1][node.r].gridFrom = node;
-      gridArray[node.c-1][node.r].farbe = color(0,200,100); //for testing
+      gridArray[node.c-1][node.r].farbe = color(0, 200, 100); //for testing
     }
   }
 }
 
-function pathFind(startNode, endNode){
+function pathFind(startNode, endNode) {
   let endFound = false;
-  
+
   //Step 0: find all the tiles/nodes adjacent to the starting node. 
   findAdjacent(startNode);
-  
-  //Step 1: loop through the elems in toSearch, calcing g & h in separate funcs, and f afterwards. 
-  for(let i = 0; i < toSearch.length; i++){ //learn how to remove stuff from toSearch between sweeps later.
-    toSearch[i].g = calcG(toSearch[i], startNode);
-    toSearch[i].h = calcH(toSearch[i], endNode);
-    toSearch[i].f = toSearch[i].g + toSearch[i].h;
-    print("G: " + toSearch[i].g + " H: " + toSearch[i].h + " F: " + toSearch[i].f);
-  }
 
+  //while (!endFound) {
+    //Step a: loop through the elems in toSearch, calcing g & h in separate funcs, and f afterwards. 
+    for (let i = 0; i < toSearch.length; i++) { //learn how to remove stuff from toSearch between sweeps later.
+      toSearch[i].g = calcG(toSearch[i], startNode);
+      toSearch[i].h = calcH(toSearch[i], endNode);
+      
+      //if (toSearch[i].h == 0) { //checks if we've reached our destination. 
+      //  endFound = true;
+      //  print("endFound");
+      //}
+      
+      toSearch[i].f = toSearch[i].g + toSearch[i].h;
+    }
+
+    //Steb b: search through toSearch to see which tile/node has the lowest F value. Then find all nodes adjacent to that tile.
+    findAdjacent(findBestTile());
+  //}
   //loop to find lowest F from the list, then check it's adj notes. 
-  //Once end found, loop through the nodes backwards checking where they came from and putting them into a global "path" array.  - all above this goes in a while loop. 
-  
+  //Once end found, loop through the nodes backwards checking where they came from and putting them into a global "path" array.  - all above this goes in a while loop.
 }
 
-function calcG(node, startNode){
-  print(startNode.c);
- return(abs(startNode.c - node.c)+abs(startNode.r - node.r));
+function calcG(node, startNode) {
+  return(abs(startNode.c - node.c)+abs(startNode.r - node.r));
 } 
 
-function calcH(node, endNode){
-return(abs(endNode.c - node.c)+abs(endNode.r - node.r));
+function calcH(node, endNode) {
+  return(abs(endNode.c - node.c)+abs(endNode.r - node.r));
+}
+
+function findBestTile(){
+let lowestF = 10000;
+    let bestTile;
+    for (let i = 0; i < toSearch.length; i++) {
+      if (toSearch[i].f < lowestF) {
+        lowestF = toSearch[i].f;
+        bestTile = toSearch[i];
+      }
+    }
+    return bestTile;
 }
