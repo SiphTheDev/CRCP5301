e7f14a91fe0ff9fe22f9501d79ca328c0466889e
SiphTheDev <siphimir@povingames.com>
2020-11-08 18:25:29 -0600

    Added constructor, move, and render methods for Arrow class.

--- VI-Project Ascent/ProjectAscent/Arrow.js
+++ VI-Project Ascent/ProjectAscent/Arrow.js
@@ -1,14 +1,26 @@
 class Arrow{
- constructor(){
-   //node where starts
-   //enemy it targets
+ constructor(x, y, target){
+   this.x = x;
+   this.y = y;
+   this.target = target;
  }
  
- /*move{
-   advances towards coordinates of targetted Enemy
- }*/
+ move(){
+   if(this.target.node.x+25 > this.x){
+     this.x++;
+   }else if(this.target.node.x+25 < this.x){
+     this.x --;
+   }
+   
+   if(this.target.node.y+25 > this.x){
+     this.y++;
+   } else if(this.target.node.y+25 < this.y){
+     this.y--;
+   }
+ }
  
- /*render{
-   image();
- }*/
+ render(){
+   ellipse(this.x, this.y, 20);
+   //image();
+ }
 }
