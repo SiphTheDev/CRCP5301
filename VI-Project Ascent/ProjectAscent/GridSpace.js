580e0449ce2d4fe79864725e685c2b02b197fcd1
SiphTheDev <siphimir@povingames.com>
2020-11-17 10:16:58 -0600

    Stage Walls now surround stage

--- VI-Project Ascent/ProjectAscent/GridSpace.js
+++ VI-Project Ascent/ProjectAscent/GridSpace.js
@@ -20,18 +20,25 @@ class GridSpace {
     //noStroke();
     if (this.type == 0) { //Empty spaces
       noFill();
-    } else if (this.type == 1) { //Borders & Walls
-      if (this.c == 2 && this.r > 1) { //left walls
-        image(gridSpriteSheet, this.c*50, this.r*50, 50, 50, 0, 256, 128, 128);
-      } else if (this.c == 25 && this.r > 1) {//right walls
-        image(gridSpriteSheet, this.c*50, this.r*50, 50, 50, 320, 256, 128, 128);
-      } else if(this.c > 2 && this.c < 25 && this.r > 0){//top & bottom walls
-        image(gridSpriteSheet, this.c*50, this.r*50, 50, 50, 128, 64, 128, 128);
-      } else if(this.c == 2 && this.r == 1){ //top left corner
+    }  else if (this.type == 1) { //Borders & Walls
+      
+      if(this.c == 2 && this.r == 0){ //top left corner
         image(gridSpriteSheet, this.c*50, this.r*50, 50, 50, 0, 64, 128, 128);
-      } else if(this.c == 25 && this.r == 1){ //top right corner
+      } else if(this.c == 25 && this.r == 0){ //top right corner
         image(gridSpriteSheet, this.c*50, this.r*50, 50, 50, 320, 64, 128, 128);
-      }else{
+      } else if(this.c == 2 && this.r == 13){ //bottom left corner;
+        image(gridSpriteSheet, this.c*50, this.r*50, 50, 50, 0, 384, 128, 128);
+      } else if(this.c == 25 && this.r == 13){ //bottom right corner;
+        image(gridSpriteSheet, this.c*50, this.r*50, 50, 50, 320, 384, 128, 128);
+      } else if (this.c == 2 && this.r > 0) { //left walls
+        image(gridSpriteSheet, this.c*50, this.r*50, 50, 50, 0, 256, 128, 128);
+      } else if (this.c == 25 && this.r > 0) {//right walls
+        image(gridSpriteSheet, this.c*50, this.r*50, 50, 50, 320, 256, 128, 128);
+      } else if(this.c > 1 && this.c < 25 && this.r == 0){//top walls
+        image(gridSpriteSheet, this.c*50, this.r*50, 50, 50, 128, 64, 128, 128);
+      } else if(this.c > 1 && this.c < 25 && this.r == 13){//bottom walls
+        image(gridSpriteSheet, this.c*50, this.r*50, 50, 50, 128, 384, 128, 128);
+      } else {
         noStroke();
         noFill();
         rectMode(CORNER);
